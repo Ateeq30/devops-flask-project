6ea47b67191c7fb70d3e9affd295cd0d81c2902f
Ubuntu <ubuntu@ip-172-31-45-62.ec2.internal>
2026-01-20 11:10:41 +0000

    update the...

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello this is an update"
+    return "i am ateeq malik"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
