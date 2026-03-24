b98a8b3fa688a605846d9ae70bc768fa5b4cbbcf
Ateeq30 <rizvi_malik786@hotmail.com>
2026-03-24 23:16:00 +0000

    commit from project1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "check again now 1.1"
+    return "pipeline flow 1.2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
